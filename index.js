20547d1c714612b8cb28df5f1e8c64c9f60aff40
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-02-23 16:06:57 +0700

    update Code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 var bodyParser = require('body-parser')
 const path = require("path")
 
-const base_url = process.env.BASE_URL || "http://node59006-env-4702943.proen.app.ruk-com.cloud";
+const base_url = "http://node59006-env-4702943.proen.app.ruk-com.cloud";
 
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
